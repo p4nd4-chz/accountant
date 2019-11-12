03ec55c958efa734556b5a836dfad248f65e2a59
p4nd4-chz <57072367+p4nd4-chz@users.noreply.github.com>
2019-11-12 13:51:41 -0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+#! /bin/bash
+# #########################################################
+# Author: p4dl0k
+# This is a user input calculator
+# This is for practice
+# 
+# #########################################################
+
+
+menuOption = input (" (+)add (-)subtract (*)multiple (/)divide: ") #user select math type
+firstNumber = int(input("First number: ")) #user input first number
+secondNumber = int(input("Second number: ")) #user input second number
+
+if (menuOption == '+'):
+   print(firstNumber + secondNumber)
+elif (menuOption == '-'):
+   print(firstNumber - secondNumber)
+elif (menuOption == '*'):
+   print(firstNumber * secondNumber)
+elif (menuOption == '/'):
+   print(firstNumber / secondNumber)
+else:
+   print("You have typed something unexpected")
+   
+   
