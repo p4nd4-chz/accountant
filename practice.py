fc18c979447e38a42404559f57edf9ca37993303
p4nd4-chz <57072367+p4nd4-chz@users.noreply.github.com>
2019-11-14 14:27:47 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -20,3 +20,19 @@ for i in range(4):
   result += nr
 print("The result of adding numbers is: ", result)
 
+
+#while loops and for
+
+result = 0
+i = 0
+
+while i < 3:
+  x = int(input("Enter a positive number"))
+  if (x>0):
+    result += x
+  else:
+    print("NONONO! I said a POSITIVE number")
+    continue
+  print("Current addition result= ", result)
+  i += 1
+  
