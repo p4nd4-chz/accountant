bae86b89946978229528f9d1827be6c7eadde5a4
p4nd4-chz <57072367+p4nd4-chz@users.noreply.github.com>
2019-11-14 22:46:24 -0800

    Update guessTheNumber.py

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,12 +1,15 @@
 #GUESS NUMBER
 from random import randint
 
-sNumber = (randint(1,10)) #secret number
+sNumber = (randint(1,100)) #secret number
 gNumber = '' #guessed number
 
 while gNumber != sNumber:
-  gNumber = int(input("Guess the number between 1 and 10! "))
-  if int(gNumber == sNumber):
-    print("Congratulations! You got it!")
+  gNumber = int(input("Guess the number between 1 and 100! "))
+  if int(gNumber > sNumber):
+    print("Too high!")
+  elif int(gNumber < sNumber):
+    print("Too low!")   
   else:
-    print("wrong!")
+    print("Congratulations!")
+
